745ad97d2ba0612db22bc7c422d8835005103b38
Thedongraphix <chrisoketchcn@gmail.com>
2025-02-18 13:56:01 +0300

    Video

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -14,7 +14,7 @@ const Video = () => {
       <div className="container">
         <SectionTitle
           title="We are ready to help"
-          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+          paragraph="There are many narratives around crypto payments but at Edupay we make this process as seamless as possible."
           center
           mb="80px"
         />
@@ -54,7 +54,7 @@ const Video = () => {
         autoplay={true}
         start={true}
         isOpen={isOpen}
-        videoId="L61p2uyiMSo"
+        videoId="Hw-vhdO67q0"
         onClose={() => setOpen(false)}
       />
 
